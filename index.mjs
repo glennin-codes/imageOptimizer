6a83300d75ff0c5c3f621f239d9ebd63e81b8cec
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-06-21 10:52:57 +0300

    changed lot

--- index.mjs
+++ index.mjs
@@ -1,49 +1,77 @@
 import express from 'express';
 import sharp from 'sharp';
-import { readFileSync } from 'fs';
+import { fsync, readFileSync } from 'fs';
 import { log } from 'console';
+import os from 'os'
+import fs from "fs"
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import multer from 'multer';
 
 
 const app=express();
 const port=3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const upload=multer({dest:'uploads/'});
+app.set('view engine', 'ejs');
+app.set('views', `${__dirname}/views`); 
 app.get('/',(req,res)=>{
-    res.send("server working properly");
+  const message=`server working properly ${os.hostname()}`
+  const formats = ['jpeg', 'jpg','png', 'webp', 'avif', 'gif', 'svg', 'tiff'];
+  res.render(
+        'form',{message:{
+          message,
+          formats
 
+        }}
+    )
+log(message);
 });
+
 app.listen(port,()=>{
     console.log(`server is listening on http://localhost:${port}`);
 })
 
-app.get('/resize', async (req, res) => {
+app.post('/upload',upload.single('image'),  async (req, res) => {
    try {
   
-    const { file } = req.query;
-    const filePath = file.split('?')[0];
+    const file  = req.file.path;
+   
+    log(file);
+    const width = parseInt(req.body.width);
+    const quality = parseInt(req.body.quality) || 80; // Set default quality to 80
 
-    const width = parseInt(req.query.width);
-    const quality = parseInt(req.query.quality) || 80; // Set default quality to 80
+    const height = parseInt(req.body.height);
+    let format = req.body.format;
+   // Read the file from the file system
+   const imageBuffer = fs.readFileSync(file);
 
-    const height = parseInt(req.query.height);
-    let format = req.query.format;
-  
     // If format is not provided, detect it based on the file extension
     if (!format) {
       const fileExtension = file.split('.').pop().toLowerCase();
       format = fileExtension === 'jpg' ? 'jpeg' : fileExtension;
     }
+    if (!height || !width) {
+      throw new Error(`Please provide ${!height ? 'height' : 'width'}`);
+    }
+    console.log(imageBuffer);
   
-    const image = await sharp(filePath)
-    .resize(width, height,
-            {
-              crop:"inside"
-            }
+    const image = await sharp(imageBuffer)
+    .resize(width, height,{
+
+      fit: sharp.fit.inside,
+      withoutEnlargement: true
+    },
+ 
+    
       )
-     .toFormat(format)
-      .toBuffer();
-     
-    res.set('Content-Type', `image/${format}`);
-    res.send(image);
-    const originalSize=readFileSync(filePath).byteLength
+     .toFormat(format,{ quality })
+      .toFile();
+      const outputImagePath = 'resized/' + image // Destination path for resized image
+
+  
+    const originalSize=readFileSync(file).byteLength
 
  const fileSizeBytes=image.byteLength;
    const fileSizeInKB=fileSizeBytes / 1024;
@@ -52,12 +80,17 @@ app.get('/resize', async (req, res) => {
     console.log(`Rendered  Original image size: ${originalSize / 1024} KB  ${(originalSize / 1024) / 1024}MB`);
   
     console.log(`Rendered final image size: ${fileSizeInKB} KB (${fileSizeInMB} MB)`);
-
+  // res.set('Content-Type', `image/${format}`);
+    // Pass any relevant information to the rendered view
+    res.render('success', { imageUrl:outputImagePath,
+      message1:` Original image size was : ${originalSize / 1024} KB  ${(originalSize / 1024) / 1024}MB`,
+      message2:`Rendered final image size as: ${fileSizeInKB} KB (${fileSizeInMB} MB)`,
+  });
     
     
    } catch (error) {
     console.error(` an error occured ${error}`);
-    
+    res.status(404).send(error.message);
     
    }
   });
