3d95b3f4e570119b8a1f8c540985339b317b8776
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-06-21 15:25:20 +0300

    changing stuffs

--- index.mjs
+++ index.mjs
@@ -2,6 +2,7 @@ import express from 'express';
 import sharp from 'sharp';
 import { fsync, readFileSync } from 'fs';
 import { log } from 'console';
+import { v4 as uuidv4 } from 'uuid';
 import os from 'os'
 import fs from "fs"
 import { dirname } from 'path';
@@ -15,7 +16,17 @@ app.use(express.static('public'));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const upload=multer({dest:'uploads/'});
+const storage = multer.diskStorage({
+  destination: 'uploads/',
+  filename: function (req, file, cb) {
+    const fileExtension = file.originalname.split('.').pop().toLowerCase();
+    const uniqueFileName = `${uuidv4()}.${fileExtension}`;
+    cb(null, uniqueFileName);
+  }
+});
+
+const upload = multer({ storage: storage });
+
 app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/views`); 
 app.get('/',(req,res)=>{
@@ -39,21 +50,33 @@ app.listen(port,()=>{
 app.post('/upload', upload.single('image'), async (req, res) => {
   try {
     const file = req.file.path;
+    const files = req.file;
+const originalFilename = files.originalname;
+log("original",originalFilename);
+
+const fileExtension = originalFilename.split('.').pop().toLowerCase();
+console.log('File format:', fileExtension);
+
     const width = parseInt(req.body.width);
     const quality = parseInt(req.body.quality) || 80;
     const height = parseInt(req.body.height);
     let format = req.body.format;
+    log("file",file)
 
     if (!format) {
       const fileExtension = path.extname(file).toLowerCase().substring(1);
       format = fileExtension === 'jpg' ? 'jpeg' : fileExtension;
     }
+    if(format==='tiff'){
+      throw new Error ('format tiff is not supported currently for processing')
+    }
     if (!height || !width) {
       throw new Error(`Please provide ${!height ? 'height' : 'width'}`);
     }
 
-    const outputImagePath = path.join(__dirname, 'public', `output.${format}`);
-
+    const uniqueSuffix = Date.now(); // Generate a unique timestamp as the suffix
+    const outputImagePath = path.join(__dirname, 'public', `output_${uniqueSuffix}.${format}`);
+    
     await sharp(file)
       .resize(width, height, {
         fit: sharp.fit.inside,
@@ -71,14 +94,24 @@ app.post('/upload', upload.single('image'), async (req, res) => {
     const { width: renderedWidth, height: renderedHeight } = await sharp(outputImagePath).metadata();
 
     res.render('success', {
-      imageUrl: outputImagePath,
+      imageUrl: `output_${uniqueSuffix}.${format}`,
       message1: `Original image size: ${originalSize / 1024} KB (${(originalSize / 1024) / 1024} MB)`,
       message2: `Rendered final image size: ${fileSizeInKB} KB (${fileSizeInMB} MB)`,
     });
     log('path',outputImagePath)
   } catch (error) {
     console.error(`An error occurred: ${error}`);
-    res.status(404).send(error.message);
+    
+      if(error.message && error.message==='Expected one of: heic, heif, avif, jpeg, jpg, jpe, tile, dz, png, raw, tiff, tif, webp, gif, jp2, jpx, j2k, j2c, jxl for format but received svg of type string'){
+        res.status(404).render('error',{message:"svg not supported currently "})
+        
+      }else{
+        res.status(404).render("error",{
+          message:error.message
+        })
+      }
+
+ 
   }
 });
 
